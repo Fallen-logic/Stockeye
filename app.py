4e55b2b16104c26b6f5a0cb06c4bc3e0dc424d78
Fallen-logic <anshkatiyar4105@gmail.com>
2026-03-08 01:46:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import yfinance as yf
+import os
 
 app = Flask(__name__)
 CORS(app)  # Allow all origins so your dashboard can call this API
